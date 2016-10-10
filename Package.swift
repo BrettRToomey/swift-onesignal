a6085a0b575f015262a7ca024718d16cf762d2ed
Brett Toomey <brettcwx@gmail.com>
2016-10-10 12:24:16 -0700

    Updated url to be friendly with Vapor projects.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftOneSignal",
     dependencies: [
-        .Package(url: "https://github.com/vapor/engine", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/engine.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/json.git", majorVersion: 1)
     ]
 )
